9b8467bafbc94bd4c8c4372ceb0d071f63bc4bd8
Elsa Rivera <elsariveramarchant@gmail.com>
2024-04-24 00:13:46 -0400

    prueba

--- js/borrar.js
+++ js/borrar.js
@@ -1,22 +1,23 @@
 
 let conteoAves=0;
 function diezAves (numeroMisterio) {
-    for (let i=0;i<10;i++){
-        conteoAves++;
-        let continuar= confirm(numeroMisterio+" misterio"+`
-                              `+conteoAves+` Cuenta: `+`
-        Dios te salve Maria, llena eres de gracia, el Señor es contigo. Bendita eres entre todas las mujeres, y bendito es el fruto de tu vientre Jesús. `+`
-        Santa Maria, madre de Dios, ruega por nosotros pecadores, ahora, y en la hora de nuestra muerte, amén.
+    // for (let i=0;i<10;i++){
+    //     conteoAves++;
+    //     let continuar= confirm(numeroMisterio+" misterio"+`
+    //                           `+conteoAves+` Cuenta: `+`
+    //     Dios te salve Maria, llena eres de gracia, el Señor es contigo. Bendita eres entre todas las mujeres, y bendito es el fruto de tu vientre Jesús. `+`
+    //     Santa Maria, madre de Dios, ruega por nosotros pecadores, ahora, y en la hora de nuestra muerte, amén.
         
-        Peticiones: `);
-        if (!continuar) {
-            alert("Has decidido no continuar con el rosario.");
-            conteoAves = 0; // Resetea el conteo de aves
-            return false; // Sale de la función diezAves
-        }
+    //     Peticiones: `);
+    //     if (!continuar) {
+    //         alert("Has decidido no continuar con el rosario.");
+    //         conteoAves = 0; // Resetea el conteo de aves
+    //         return false; // Sale de la función diezAves
+    //     }
          
-    }
-    conteoAves=0;
+    // }
+    // conteoAves=0;
+    alert(numeroMisterio+"hola")
     return true; 
 }
 diezAves(3);
